b24351f5f324445037f0e4e51e536e0d68251f01
Rojan <rojandhimal1@gmail.com>
2019-01-18 17:50:14 +0545

    update program that detects line

--- lanes.py
+++ lanes.py
@@ -7,13 +7,24 @@ def region_of_interest(image):
 
     height = image.shape[0]  #extracting eight from original shape of image
     polygonals = np.array([  
-        [(200, height), (1100, height), (550,25)]
+        [(200, height), (1100, height), (550,250)]
     ])
     mask = np.zeros_like(image)         #making black image using np zeros image 
     cv2.fillPoly(mask,polygonals,255)  #drawing poygons 
 
     masked_image = cv2.bitwise_and(image, mask)   #bit wise ADN of image and mask
     return masked_image
+
+def display_lines(img,lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+    return line_image
+
+
+
 
 def canny(image):
     #this is the function to detect edge from image
@@ -27,11 +38,19 @@ def canny(image):
 
 
 #importing image 
-image = cv2.imread("lane.jpg")
+image = cv2.imread("images/lane.jpg")
 lane_image = np.copy(image)  #copying lane image to make change in copy image
 
 canny = canny(lane_image)   #calling canny eade detection function
 cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,np.array([]),minLineLength=40, maxLineGap=5)
 
-cv2.imshow("cropped image ", cropped_image)
+#linned image 
+line_image = display_lines(lane_image,lines)
+
+#image and lines combined
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1) 
+
+
+cv2.imshow("Linned image", combo_image)  #line image
 cv2.waitKey(0)
